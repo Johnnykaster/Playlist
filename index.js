e038c09c746aed52a1e71163b4192b6e6e1028af
JohnnyKaster <johnnykaster@rocketmail.com>
2020-02-14 18:18:16 +0000

    Its working, just need to style it

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let playList = []
+let playList = [];
 
 function displayList() {
+    $(".playList").html("");
   playList.forEach(function(show) {
     $(".playList").append("<li>" + show + "</li>")
   })
 };
 
 var input = document.getElementById("addOn");
-
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     playList.push($("#addOn").val());
-    $(".playList").append("<li>" + playList(playList.length()) + "<li>");
+    displayList();
+    console.log(playList)
   }
 });
