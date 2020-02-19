8e8a7ebcabe7e2fb00bf5315587ab54888e116b5
JohnnyKaster <johnnykaster@rocketmail.com>
2020-02-19 02:26:52 +0000

    The local storage is now working (pretty happy with it). All thats left is styling

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-let playList = []
-localStorage.setItem('items', JSON.stringify(playList))
-const data = JSON.parse(localStorage.getItem('items'))
+let playList = JSON.parse(localStorage.getItem("playArray"));
+if (playList === null) {
+    playList = [];
+} else {
+    console.log(playList)
+}
+console.log(playList)
 
 function displayList() {
     $(".playList").html("");
-  data.forEach(item => {
+  playList.forEach(item => {
       $(".playList").append("<li>" + item + "</li>")
+      console.log(item);
 })
 };
+
+displayList();
 
 var input = document.getElementById("addOn");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     playList.push($("#addOn").val());
-    localStorage.setItem('items', JSON.stringify(playList))
     displayList();
+    localStorage.setItem("playArray", JSON.stringify(playList))
     console.log(playList)
   }
 });
